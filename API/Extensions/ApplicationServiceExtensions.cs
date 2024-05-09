bc854988059316b2217afcad0efaf7143824a629
Tobias Schuler <tobiasschuler@private.com>
2024-05-09 17:14:29 +0200

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
